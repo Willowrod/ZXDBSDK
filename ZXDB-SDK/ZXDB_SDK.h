f115c68cf413f3391e24518e2a86de0ed51924b9
Mike Hall <mike@willowrod.com>
2021-05-11 10:50:34 +0100

    Initial Commit

---
+++ ZXDB-SDK/ZXDB_SDK.h
@@ -0,0 +1,18 @@
+//
+//  ZXDB_SDK.h
+//  ZXDB-SDK
+//
+//  Created by Mike Hall on 11/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZXDB_SDK.
+FOUNDATION_EXPORT double ZXDB_SDKVersionNumber;
+
+//! Project version string for ZXDB_SDK.
+FOUNDATION_EXPORT const unsigned char ZXDB_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZXDB_SDK/PublicHeader.h>
+
+
